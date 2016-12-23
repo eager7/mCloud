2fd92d17bd0804c546e2e1cd0f125b5051162294
eager7 <panchagntao@gmail.com>
2016-12-23 05:44:17 -0800

    测试成功pub类，在创建成功socket后，需等待一下再发送，否则第一个消息将丢失。

--- zigbee_cloud/mZeromq.py
+++ zigbee_cloud/mZeromq.py
@@ -3,27 +3,47 @@
 __author__ = 'Chandler.Pan'
 __metaclass__ = type
 
-import zmq
+import zmq, time
 
-PULL_ADDRESS = "127.0.0.1:"
-PULL_PORT = 5556
+PUB_ADDRESS = "tcp://127.0.0.1:5555"
+PULL_ADDRESS = "tcp://127.0.0.1:5556"
 
 
 class mZeroMqPullPush():
     '''
-    这个类用作服务器接收客户端的请求，服务器是Pull类型，客户端是Push类型
-    '''
-    def __init__(self):
+        这个类用作服务器接收客户端的请求，服务器是Pull类型，客户端是Push类型，因此可以实现多对一'''
+    def __init__(self, address):
         super(mZeroMqPullPush, self).__init__()
         self.context = zmq.Context()
         self.receiver = self.context.socket(zmq.PULL)
-        self.receiver.connect("tcp://" + PULL_ADDRESS + str(PULL_PORT) )
+        self.receiver.connect(address)
 
-    def Recv(self):
+    def Receive(self):
         return self.receiver.recv_json()
 
 
+class mZeroMqPubSub():
+    '''
+         这个类用作服务器向网关请求消息时使用，网关端用sub，因此可实现一对多'''
+    def __init__(self, address):
+        super(mZeroMqPubSub, self).__init__()
+        try:
+            self.context = zmq.Context()
+            self.publisher = self.context.socket(zmq.PUB)
+            self.publisher.bind(address)
+        except Exception, e:
+            print e
+
+    def Publish(self, message):
+        print 'send:', message
+        self.publisher.send_json(message)
+
 if __name__ == '__main__':
     print "test self"
-    mClass = mZeroMqPullPush()
-    print mClass.Recv()
+    #mClass = mZeroMqPullPush(PULL_ADDRESS)
+    #print mClass.Receive()
+
+    mPub = mZeroMqPubSub(PUB_ADDRESS)
+    time.sleep(1)
+    mPub.Publish('{\"name\":\"BeJson\"}')
+    mPub.Publish('{\"name\":\"BeJson\"}')
